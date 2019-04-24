da7e82b94b8e9404dcea393d34b2fe981d690b8e
briandotcom0 <briandotcom0@gmail.com>
2019-04-24 12:17:09 -0500

    initialize variables to 0 and hope that doesnt break actual assignment

--- callbacks.py
+++ callbacks.py
@@ -1,19 +1,19 @@
 import RPi.GPIO as GPIO
 import pins
-import krpc
-import krpc
+# import krpc  #???
 
-conn
-vessel
+conn=0
+vessel=0
 
 def setup(c,v):
     conn=c
     vessel=v
+    pins.setup()
 
 def rcs(channel):
     vessel.control.rcs=GPIO.input(channel)
-    GPIO.output(pins.rcsout,GPIO.input(channel))
+    GPIO.output(pins.rcsout, GPIO.input(channel))
 
 def sas(channel):
     vessel.control.sas=GPIO.input(channel)
-    GPIO.output(pins.sasout,GPIO.input(channel))
+    GPIO.output(pins.sasout, GPIO.input(channel))
